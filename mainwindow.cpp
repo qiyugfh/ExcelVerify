ee38420968e906146d49fff681a4ed2702072bc6
guofh <guofh@tingsn.com>
2022-09-01 19:00:29 +0800

    ui处理

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QFileInfo>
+#include <QMessageBox>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    initUI();
+
+    m_recentFilePath = "D:/";
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +20,92 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::initUI()
+{
+    connect(ui->btnBrowser1, SIGNAL(clicked()), this, SLOT(onBtnBrowserClicked1()));
+    connect(ui->btnBrowser2, SIGNAL(clicked()), this, SLOT(onBtnBrowserClicked2()));
+    connect(ui->btnBrowser3, SIGNAL(clicked()), this, SLOT(onBtnBrowserClicked3()));
+}
+
+
+QString MainWindow::browseExcelFilePath(const QString &info)
+{
+    QString filePath = QFileDialog::getOpenFileName(this, info, m_recentFilePath, "Excel files(*.xlsx *.xls");
+    if(filePath.isEmpty())
+    {
+        return "";
+    }
+
+    QFileInfo fileInfo(filePath);
+    m_recentFilePath = fileInfo.absolutePath();
+
+    return filePath;
+}
+
+
+void MainWindow::onBtnBrowserClicked1()
+{
+    QString filePath = browseExcelFilePath(QString::fromLocal8Bit("选择表1 社保公积金结算单"));
+    ui->editExcelFilePath1->setText(filePath);
+}
+
+
+void MainWindow::onBtnBrowserClicked2()
+{
+    QString filePath = browseExcelFilePath(QString::fromLocal8Bit("选择表2 薪资明细表（从系统中导出的）"));
+    ui->editExcelFilePath2->setText(filePath);
+}
+
+
+void MainWindow::onBtnBrowserClicked3()
+{
+    QString filePath = browseExcelFilePath(QString::fromLocal8Bit("选择表3 薪资明细表（造册）"));
+    ui->editExcelFilePath3->setText(filePath);
+}
+
+
+void MainWindow::onBtnCheck1()
+{
+    QString filePath1 = ui->editExcelFilePath1->text().trimmed();
+    QString filePath2 = ui->editExcelFilePath2->text().trimmed();
+
+    if(false == checkFileExists(filePath1)
+    || false == checkFileExists(filePath2))
+    {
+        return;
+    }
+
+
+
+}
+
+
+void MainWindow::onBtnCheck2()
+{
+
+}
+
+
+void MainWindow::onBtnCheck3()
+{
+
+
+}
+
+void MainWindow::showInfoMessageBox(const QString &info)
+{
+    QMessageBox::information(this, QString::fromLocal8Bit("提示信息"), info, QMessageBox::Yes);
+}
+
+
+ bool MainWindow::checkFileExists(const QString &filePath)
+ {
+     if(!QFileInfo::exists(filePath))
+     {
+         showInfoMessageBox(QString::fromLocal8Bit(QString("Not exist file: %1").arg(filePath).toStdString().c_str()));
+         return false;
+     }
+
+     return true;
+ }
--- mainwindow.h
+++ mainwindow.h
@@ -15,7 +15,29 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void onBtnBrowserClicked1();
+    void onBtnBrowserClicked2();
+    void onBtnBrowserClicked3();
+    void onBtnCheck1();
+    void onBtnCheck2();
+    void onBtnCheck3();
+
+
+
+private:
+    void initUI();
+    QString browseExcelFilePath(const QString &info);
+    void showInfoMessageBox(const QString &info);
+    bool checkFileExists(const QString &filePath);
+
+
 private:
     Ui::MainWindow *ui;
+    QString         m_recentFilePath;
+    QString         m_excelFile1;
+    QString         m_excelFile2;
+    QString         m_excelFile3;
+
 };
 #endif // MAINWINDOW_H
